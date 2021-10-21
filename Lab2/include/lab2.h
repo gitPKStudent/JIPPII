4a1e8da6b1a5dbe7e196d776d0c778ffbf07ab81
Mikolaj <mik155@wp.pl>
2021-10-21 22:06:41 +0200

    LAB2

---
+++ Lab2/include/lab2.h
@@ -0,0 +1,19 @@
+#ifndef LAB2_LAB2_H
+#define LAB2_LAB2_H
+#include <string>
+int compare(std::string a, std::string b);
+double area(double a);
+double area(double a, double h);
+double area(int r);
+double random(double a, double b);
+double maxArrayValue(double * array);
+void reverse(double* array);
+void printArray(double* array);
+double average(double* a, double* b);
+void loadPositives();
+void bubleSort(double* array);
+void swap(int x, int *w);
+void divide(double a, double b);
+void convert(std::string num);
+
+#endif //LAB2_LAB2_H
